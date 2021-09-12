61fc3ea5ed90f2a3d64d8ecab031582441089c83
Ihor Tsoi <ihor.tsoi@nure.ua>
2021-09-12 06:30:50 -0700

    DEV: clean-up

--- lib/factorial_implementations_question.ex
+++ lib/factorial_implementations_question.ex
@@ -1,18 +1,2 @@
 defmodule FactorialImplementationsQuestion do
-  @moduledoc """
-  Documentation for `FactorialImplementationsQuestion`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FactorialImplementationsQuestion.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/factorial_implementations_question_test.exs
+++ test/factorial_implementations_question_test.exs
@@ -1,8 +1,4 @@
 defmodule FactorialImplementationsQuestionTest do
   use ExUnit.Case
   doctest FactorialImplementationsQuestion
-
-  test "greets the world" do
-    assert FactorialImplementationsQuestion.hello() == :world
-  end
 end
