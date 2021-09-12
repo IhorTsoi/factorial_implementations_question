cb994e7f989b26e5f03a842cc9024966de49e57c
Ihor Tsoi <ihor.tsoi@nure.ua>
2021-09-12 06:32:57 -0700

    DEV: factorial_with_reduce and factorial_with_reduce_on_chunks were implemented.

--- lib/factorial_implementations_question.ex
+++ lib/factorial_implementations_question.ex
@@ -1,2 +1,17 @@
 defmodule FactorialImplementationsQuestion do
+  def factorial_with_reduce(n)
+      when is_integer(n) and n >= 0 do
+    1..n//1
+    |> Enum.reduce(1, &*/2)
+  end
+
+  def factorial_with_reduce_on_chunks(n, chunk_size \\ 4)
+      when is_integer(n) and n >= 0 do
+    1..n//1
+    |> Stream.chunk_every(chunk_size)
+    |> Stream.map(fn list ->
+      Enum.reduce(list, 1, &*/2)
+    end)
+    |> Enum.reduce(1, &*/2)
+  end
 end
