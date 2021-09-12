f14d7a38352e1b9b9f5987f4c76c4b7957d6a27f
Ihor Tsoi <ihor.tsoi@nure.ua>
2021-09-12 10:18:40 -0700

    BENCH: factorial_with_reduce_on_ranges was added to benchmarks, inputs["5'000"] was removed.

--- benchmarks/factorial_implementations_benchmarks.exs
+++ benchmarks/factorial_implementations_benchmarks.exs
@@ -5,10 +5,12 @@ Benchee.run(
     end,
     "Factorial with reduce on chunks" => fn n ->
       FactorialImplementationsQuestion.factorial_with_reduce_on_chunks(n)
+    end,
+    "Factorial with reduce on ranges" => fn n ->
+      FactorialImplementationsQuestion.factorial_with_reduce_on_ranges(n)
     end
   },
   inputs: %{
-    "5'000" => 5_000,
     "100'000" => 100_000,
     "500'000" => 500_000
   },
