9be8f50b665f257a73209cca4fd71fcadcf63646
Ihor Tsoi <ihor.tsoi@nure.ua>
2021-09-12 06:46:29 -0700

    TEST: doctests for factorial_with_reduce and factorial_with_reduce_on_chunks were added.

--- lib/factorial_implementations_question.ex
+++ lib/factorial_implementations_question.ex
@@ -1,10 +1,28 @@
 defmodule FactorialImplementationsQuestion do
+  @doc """
+    Examples:
+
+    iex> FactorialImplementationsQuestion.factorial_with_reduce 0
+    1
+
+    iex> FactorialImplementationsQuestion.factorial_with_reduce 5
+    120
+  """
   def factorial_with_reduce(n)
       when is_integer(n) and n >= 0 do
     1..n//1
     |> Enum.reduce(1, &*/2)
   end
 
+  @doc """
+    Examples:
+
+    iex> FactorialImplementationsQuestion.factorial_with_reduce_on_chunks 0
+    1
+
+    iex> FactorialImplementationsQuestion.factorial_with_reduce_on_chunks 5
+    120
+  """
   def factorial_with_reduce_on_chunks(n, chunk_size \\ 4)
       when is_integer(n) and n >= 0 do
     1..n//1
